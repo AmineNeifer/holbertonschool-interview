78dbd5274ffc4e7d57ee1d0ed424617e6738a30d
AmineNeifer <amineneifer2000@gmail.com>
2020-07-15 15:24:36 +0100

    lockboxesss

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" contains a function that returns whether all boxes can be opened or not"""
+
+
+def canUnlockAll(boxes):
+    """ checks if there is an empty list in the middle 
+    or the beginning of the list"""
+    for i in range(len(boxes)):
+        if not boxes[i] and i < len(boxes) - 1:
+            return False
+    return True
