b42b6be211d6f771b50f92e5bd50c2b9a016ff5a
AmineNeifer <amineneifer2000@gmail.com>
2020-07-21 19:51:23 +0100

    corrected the algorithm

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,7 +5,18 @@
 def canUnlockAll(boxes):
     """ checks if there is an empty list in the middle
     or the beginning of the list"""
-    for i in range(len(boxes)):
-        if not boxes[i] and i < len(boxes) - 1:
-            return False
-    return True
+    opened = [i for i in boxes[0]]
+    closed = [i for i in range(1, len(boxes)) if i not in opened]
+    while True:
+        for index in opened:
+            test_opened = opened[:]
+            for i in boxes[index]:
+                opened.append(i)
+                try:
+                    closed.remove(i)
+                except ValueError:
+                    pass
+            if closed == []:
+                return True
+            if test_opened == opened:
+                return False
