c5353ff0fde00d539edcd01bd762a9cb2b2f699d
AmineNeifer <amineneifer2000@gmail.com>
2020-07-21 23:38:00 +0100

    corrected most of it

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,26 +5,52 @@
 def canUnlockAll(boxes):
     """ checks if there is an empty list in the middle
     or the beginning of the list"""
-    if boxes is not list:
+    if type(boxes) is not list:
         return False
-    for i in boxes:
-        if i is not list:
+    if len(boxes) == 0:
+        return False
+    try:
+        for item in boxes:
+            if type(item) is not list:
+                return False
+        if type(boxes[0][0]) is not int:
             return False
+
+    except IndexError:
+        pass
+    
     opened = [i for i in boxes[0]]
+    if not opened:
+        return False
+
     closed = [i for i in range(1, len(boxes)) if i not in opened]
     if len(boxes) == 1:
         return True
-    while True:
-        for index in opened:
-            test_opened = opened[:]
+
+
+    for index in opened:
+        if (index == 0):
+            continue
+        test_opened = opened[:]
+        print("opened = " + str(opened))
+        print("closed = " + str(closed))
+        try:
+            print("boxes[indx] = " + str(boxes[index]))
             for i in boxes[index]:
+                print(i)
                 if (i not in opened):
                     opened.append(i)
                 try:
                     closed.remove(i)
                 except ValueError:
                     pass
-            if closed == []:
-                return True
-            if test_opened == opened:
-                return False
+        except IndexError:
+            pass
+
+        if closed == []:
+            print("5ater closed msakra")
+            return True
+        if test_opened == opened:
+            print("test opened =" + str(test_opened))
+            print("5ater test = opened")
+            return False
