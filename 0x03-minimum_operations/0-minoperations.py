5494f35384a082785b970a449942852c56b49e51
AmineNeifer <amineneifer2000@gmail.com>
2020-08-12 13:26:56 +0100

    no inifinite loop anymore

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,16 +5,16 @@
 
 
 def minOperations(n):
-    """ Return min number of operations needed copy paste and get n number of Hs"""
+    """ Return min number of operations needed to get n number of Hs"""
     count = 0
     s = "H"
     if n < 2 or type(n) is not int:
         return 0
     while len(s) != n:
         if n % len(s) == 0:
+            prev = s
             s = s * 2
             count += 2
-            prev = s
         else:
             s = s + prev
             count += 1
