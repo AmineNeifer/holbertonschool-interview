31013597034f3dc2d47c83d1972298f1462e0354
AmineNeifer <amineneifer2000@gmail.com>
2020-08-12 13:17:32 +0100

    ouh

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -8,10 +8,9 @@ def minOperations(n):
     """ Return min number of operations needed copy paste and get n number of Hs"""
     count = 0
     s = "H"
-    wanted = s * n
     if n < 2 or type(n) is not int:
         return 0
-    while len(s) < n:
+    while len(s) != n:
         if n % len(s) == 0:
             s = s * 2
             count += 2
