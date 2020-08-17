ae4e18fda1be94349ae9e2807a8901463be5d146
AmineNeifer <amineneifer2000@gmail.com>
2020-08-17 15:00:53 +0100

    header file

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int check_sandpiles(int grid[3][3]);
+void topple_sandpiles(int grid[3][3]);
+void print_gridy(int grid[3][3]);
+void copy_grid(int grid1[3][3], int grid2[3][3]);
+
+#endif /* _SANDPILES_H_ */
