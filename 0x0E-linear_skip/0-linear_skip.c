c5b3bc954f4e679ccb7ff97e3ab7ad72621d082c
AmineNeifer <amineneifer2000@gmail.com>
2020-11-11 20:03:54 +0100

    betty style

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,19 +1,22 @@
 #include "search.h"
 /**
+ * linear_skip - searches for a value in a sorted skip list of integers
  *
+ * @list: pointer to the head of the skip list to search in.
+ * @value: value to search for.
  *
- *
- *
+ * Return: node that contains the value, NULL if fails.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 
 	size_t idx, idx_x;
 	skiplist_t *current = list, *express;
-	
+
 	if (!list)
-		return NULL;
-	while(current)
+		return (NULL);
+
+	while (current)
 	{
 		if (current->express)
 			express = current->express;
@@ -23,10 +26,10 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			while (express && express->next)
 				express = express->next;
 			idx = current->index;
-            idx_x = express->index;
+			idx_x = express->index;
 			printf("Value found between indexes [%lu] and [%lu]\n", idx, idx_x);
-			return search(current, value);
-			
+			return (search(current, value));
+
 		}
 		printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
 		if (express->n > value)
@@ -43,12 +46,19 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	}
 	return (NULL);
 }
-
+/**
+ * search - searchs in the nodes between the current and current->express.
+ *
+ * @list: pointer to the head of the skip list to search in.
+ * @value: value to search for.
+ *
+ * Return: node that contains the value, NULL if fails.
+ */
 skiplist_t *search(skiplist_t *list, int value)
 {
 	skiplist_t *c;
 
-	for(c = list; c; c= c->next)
+	for (c = list; c; c = c->next)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", c->index, c->n);
 		if (c->n == value)
