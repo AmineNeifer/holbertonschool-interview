b350f63a39ab06749b046b0562fd6af0a26b04b2
AmineNeifer <amineneifer2000@gmail.com>
2020-11-25 19:31:06 +0000

    readme file and rain function

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+
+""" contains rain function"""
+
+
+def rain(walls):
+    """
+    Given a list of non-negative integers representing walls of width 1,
+    calculates how much water will be retained after it rains.
+    """
+    if not walls:
+        return 0
+    water = []
+    i, j = 0, 0
+    while i < len(walls) and walls[i] == 0:
+        i += 1
+    j = i + 1
+    water = rain_helper(i, j, walls, water)
+    return sum(water)
+
+
+def rain_helper(i, j, walls, water):
+    """
+    Rain helper function find the first two number that have
+    zeroes in between and it counts the "rain"
+    """
+    if (j >= len(walls) - 1):
+        return water
+    if j < len(walls) and walls[j] != 0:
+        return rain_helper(i + 1, j + 1, walls, water)
+    while j < len(walls) and walls[j] == 0:
+        j += 1
+    if j == len(walls):
+        return water
+    width = j - i - 1
+    water.append(width * walls[i])
+    return rain_helper(j, j + 1, walls, water)
