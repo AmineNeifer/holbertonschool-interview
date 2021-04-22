570dc05483f46bfac9010a92bce11a16dd08c2bc
Amine Neifer <amineneifer2000@gmail.com>
2021-04-22 23:26:16 +0100

    first commit

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Contains island_perimeter function.
+"""
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid.
+    """
+    somme = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    for i in range(rows):
+        for j in range(cols):
+            num = 0
+            if grid[i][j] == 1:
+                try:
+                    if grid[i-1][j] == 0:
+                        num += 1 
+                except IndexError:
+                    num +=1
+                try:
+                    if grid[i+1][j] == 0:
+                        num += 1 
+                except IndexError:
+                    num +=1
+                try:
+                    if grid[i][j-1] == 0:
+                        num += 1 
+                except IndexError:
+                    num +=1
+                try:
+                    if grid[i][j+1] == 0:
+                        num += 1 
+                except IndexError:
+                    num +=1
+            somme += num
+    return somme
+
