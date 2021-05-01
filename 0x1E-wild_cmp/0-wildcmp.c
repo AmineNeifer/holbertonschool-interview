15d30cd6563fabcc0cfac6a8949628e6f27165e9
Amine Neifer <amineneifer2000@gmail.com>
2021-05-01 11:23:09 +0100

    wildcmp

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * wildcmp - compares two strings and returns
+ *
+ * @string: string to compare.
+ * @pattern: string 2 which contains wildcards.
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0
+ */
+
+int wildcmp(char *string, char *pattern)
+{
+	if (*pattern == '\0' && *string == '\0')
+		return (1);
+
+	if (*pattern == *string)
+		return (wildcmp(string + 1, pattern + 1));
+
+	if (*pattern == '*')
+		return (wildcmp(string + 1, pattern) || wildcmp(string, pattern + 1));
+
+	return (0);
+}
---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,6 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int wildcmp(char *string, char *pattern);
+
+#endif /* _HOLBERTON_H_ */
